427cd47db0a428633d19c5f62071638aec0080ba
Iipal <malkoleyplay@gmail.com>
2019-04-12 23:37:35 +0300

    added protect fro Surface in sdl_pixelput

--- libftsdl/srcs/sdl_pixelput.c
+++ libftsdl/srcs/sdl_pixelput.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 20:02:04 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/04/12 17:39:35 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/04/12 23:36:53 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ inline void	sdl_pixelput(SDL_Surface *surf, int32_t x, int32_t y, Color clr)
 {
 	int32_t	*pixels;
 
-	pixels = surf->pixels;
-	if (0 <= x && 0 <= y && y < surf->h && x < surf->w)
-		pixels[y * surf->w + x] = sdl_clr_convert_rgb(clr);
+	if (surf)
+	{
+		pixels = surf->pixels;
+		if (0 <= x && 0 <= y && y < surf->h && x < surf->w)
+			pixels[y * surf->w + x] = sdl_clr_convert_rgb(clr);
+	}
 }
