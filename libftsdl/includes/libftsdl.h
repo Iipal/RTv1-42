1c02bde6d2407ce5853b29674b727d9843c2c351
Iipal <malkoleyplay@gmail.com>
2019-04-12 20:20:40 +0300

    Added prototypes for latest added func

--- libftsdl/includes/libftsdl.h
+++ libftsdl/includes/libftsdl.h
@@ -6,12 +6,14 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 19:59:09 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/04/12 17:39:11 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/04/12 20:08:43 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTSDL_H
 # define LIBFTSDL_H
+
+# include "../../libft/includes/libft.h"
 
 # ifdef __APPLE__
 #  include "../../frameworks/SDL2.framework/Headers/SDL.h"
@@ -19,9 +21,6 @@
 # ifdef __linux__
 #  include <SDL2/SDL.h>
 # endif
-
-# include "../../libft/includes/libft.h"
-# include <stdint.h>
 
 # define E_SURFPXL  " ERROR: Broken SDL_Surface load. ->pixels invalid."
 
@@ -43,24 +42,32 @@ struct	s_color
 	int16_t	b;
 };
 
-# define SDL typedef struct s_sdl	Sdl;
-# define SDL_COLOR	typedef struct s_color	Color;
+# define SDL    typedef struct s_sdl    Sdl;
+# define COLOR  typedef struct s_color  Color;
 
-SDL_COLOR;
+COLOR;
 SDL;
 
-bool		sdl_init(Sdl *sdl, int32_t width, int32_t height, cstring title);
+bool	sdl_init(Sdl *sdl, int32_t width, int32_t height, cstring title);
 
-void		sdl_pixelput(SDL_Surface *surf, int32_t x, int32_t y, Color clr);
+void	sdl_pixelput(SDL_Surface *surf, int32_t x, int32_t y, Color clr);
 
-Uint8		sdl_clr_inrange(int16_t color);
-Uint32		sdl_clr_convert_rgb(Color src);
+Uint8	sdl_clr_inrange(int16_t color);
 
-void		sdl_clrs_add(Color *dst, const Color add);
+Uint32	sdl_clr_convert_rgb(Color src);
 
-void		sdl_clr_bright_inc(Color *dst, Uint8 amount);
-void		sdl_clr_bright_dec(Color *dst, Uint8 amount);
+Color	*sdl_gradient(const Color *start, const Color *end, int32_t length);
 
-void		sdl_free(Sdl **sdl);
+bool	sdl_clrs_equal(const Color *c1, const Color *c2);
+
+void	sdl_clrs_add(Color *dst, const Color add);
+void	sdl_clrs_div(Color *dst, const Color div);
+void	sdl_clrs_sub(Color *dst, const Color sub);
+void	sdl_clrs_mul(Color *dst, const Color mul);
+
+void	sdl_clr_bright_inc(Color *dst, float amount);
+void	sdl_clr_bright_dec(Color *dst, float amount);
+
+void	sdl_free(Sdl **sdl);
 
 #endif
