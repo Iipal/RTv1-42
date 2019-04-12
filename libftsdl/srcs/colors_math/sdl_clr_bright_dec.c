f552be611119ba3465233381d04237108970b2a7
Iipal <malkoleyplay@gmail.com>
2019-04-12 20:18:26 +0300

    improved funcs sdl_clr_bright_dec\inc to work with float

--- libftsdl/srcs/colors_math/sdl_clr_bright_dec.c
+++ libftsdl/srcs/colors_math/sdl_clr_bright_dec.c
@@ -6,22 +6,26 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 15:44:45 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/04/12 17:37:35 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/04/12 20:12:11 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftsdl.h"
 
-inline void	sdl_clr_bright_dec(Color *dst, Uint8 amount)
+inline void	sdl_clr_bright_dec(Color *dst, float amount)
 {
-	if (1 < amount)
+	if (.0f < amount)
 	{
 		if (!dst->r && !dst->b && !dst->g)
 			*dst = (Color) {amount, amount, amount};
 		else
+		{
+			if (1.0f > amount)
+				amount += 1.0f;
 			*dst = (Color){
 				sdl_clr_inrange(dst->r / amount),
 				sdl_clr_inrange(dst->g / amount),
 				sdl_clr_inrange(dst->b / amount)};
+		}
 	}
 }
--- libftsdl/srcs/colors_math/sdl_clr_bright_inc.c
+++ libftsdl/srcs/colors_math/sdl_clr_bright_inc.c
@@ -6,22 +6,26 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 15:52:31 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/04/12 17:37:43 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/04/12 20:13:58 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftsdl.h"
 
-inline void	sdl_clr_bright_inc(Color *dst, Uint8 amount)
+inline void	sdl_clr_bright_inc(Color *dst, float amount)
 {
-	if (1 < amount)
+	if (.0f < amount)
 	{
 		if (!dst->r && !dst->b && !dst->g)
 			*dst = (Color) {amount, amount, amount};
 		else
+		{
+			if (1.0f > amount)
+				amount += 1.0f;
 			*dst = (Color){
 				sdl_clr_inrange(dst->r * amount),
 				sdl_clr_inrange(dst->g * amount),
 				sdl_clr_inrange(dst->b * amount)};
+		}
 	}
 }
