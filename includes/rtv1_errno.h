9ae10b071c689df78cf6758d9f0c80a609aa8297
Iipal <malkoleyplay@gmail.com>
2019-04-12 10:43:58 +0300

    changed errno message name

--- includes/rtv1_errno.h
+++ includes/rtv1_errno.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 14:05:25 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/04/11 12:01:53 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/04/11 19:22:10 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 # define E_FNLESS   ERR "Too short file name."
 # define E_FILEXT   ERR "Invalid scene file extention. (.rtv1)"
 # define E_FILE     ERR "Scene setup file doesnt exist."
-# define E_FREAD    ERR "File is empty."
+# define E_FEMPTY   ERR "File is empty."
 # define E_WINSIZE  ERR "Invalid window sizes. Less than 400px or too big."
 # define E_ISYNTAX  ERR "Invalid scene file syntax."
 
