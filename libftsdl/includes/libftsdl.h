eb8a08e6fabf3e80d8452224285d5829dd643d92
Iipal <malkoleyplay@gmail.com>
2019-04-12 23:38:05 +0300

    added comments in libftsdl.h for main funcs

--- libftsdl/includes/libftsdl.h
+++ libftsdl/includes/libftsdl.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 19:59:09 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/04/12 23:27:16 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/04/12 23:37:09 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,30 @@ struct	s_sdl
 
 SDL;
 
+/*
+**	Initialize SDL2.
+**	\param sdl: Pointer to already allocated Sdl.
+**	\param width: Window width what will created.
+**	\param height: Window height what will created.
+**	\param title: Title for window.
+*/
 bool	sdl_init(Sdl *sdl, int32_t width, int32_t height, cstring title);
 
+/*
+**	Free all data in Sdl*.
+*/
 void	sdl_free(Sdl **sdl);
 
+/*
+**	mlx_pixelput implementation on SDL2 for copy workflow from MLX library.
+**	\param surf: Surface in which will set color \param clr.
+**	\param x: X position in surface->pixels.
+**	\param y: Y position in surface->pixels.
+**	\param clr: Color which will set on \param x \param y position.
+**
+**	Protected from segfault when x && y pixel doesnt exist on Surface,
+**	and if Surface doesnt exist too.
+*/
 void	sdl_pixelput(SDL_Surface *surf, int32_t x, int32_t y, Color clr);
 
 #endif
