789e8940ef32bd9d66e817f821120054cbf55629
Iipal <malkoleyplay@gmail.com>
2019-04-13 15:32:29 +0300

    improved sdl_clr_bright_inc\dec, and yes, inited new branch

--- libftsdl/srcs/colors_math/clr/sdl_clr_bright_dec.c
+++ libftsdl/srcs/colors_math/clr/sdl_clr_bright_dec.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 15:44:45 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/04/13 09:51:46 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/04/13 15:30:42 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,8 @@
 inline void	sdl_clr_bright_dec(Color *dst, float percent)
 {
 	if (.0f < percent)
-	{
-		if (!dst->r && !dst->b && !dst->g)
-			*dst = (Color) {1, 1, 1};
-		else
-			*dst = (Color) {
-				INRANGE(dst->r + (dst->r * (percent / 100))),
-				INRANGE(dst->g + (dst->g * (percent / 100))),
-				INRANGE(dst->b + (dst->b * (percent / 100)))};
-	}
+		*dst = (Color) {
+			INRANGE((!dst->r ? 1 : dst->r) - (dst->r * (percent / 100.0f))),
+			INRANGE((!dst->g ? 1 : dst->g) - (dst->g * (percent / 100.0f))),
+			INRANGE((!dst->b ? 1 : dst->b) - (dst->b * (percent / 100.0f)))};
 }
