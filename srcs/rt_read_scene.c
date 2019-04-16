086aede2d6bff3c22f1c789ad0cec7d19748de19
_Ipal Desse <ipal@localhost.localdomain>
2019-04-16 19:54:37 +0300

    validating fix

--- srcs/rt_read_scene.c
+++ srcs/rt_read_scene.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 16:47:30 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/04/16 19:09:46 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/04/16 19:30:53 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,18 @@
 
 static bool	rt_scam(Environment *env, string line)
 {
-	double	**vars;
+	double	**dst;
 	int32_t	i;
 	string	tmp;
 
-	vars = (double*[]){&env->cam.pos.x, &env->cam.pos.y, &env->cam.pos.z,
+	dst = (double*[]){&env->cam.pos.x, &env->cam.pos.y, &env->cam.pos.z,
 						&env->cam.dir.x, &env->cam.dir.y, &env->cam.dir.z};
 	tmp = line;
 	line += ft_skip_to_blank(line);
-	IFDOR(*line != ' ', ft_strdel(&tmp), false);
+	IFDOR(*line++ != ' ', ft_strdel(&tmp), false);
 	IFDOR(!*line || !ft_isdigit(*line), ft_strdel(&tmp), false);
 	i = -1;
-	while (MAX_CAM_PARAMS > ++i)
-	{
-		line += ft_digits(*vars[i] = ft_atoi(line));
+	while (MAX_CAM_PARAMS > ++i && (line += ft_digits(*dst[i] = ft_atoi(line))))
 		if (MAX_CAM_PARAMS != i + 1)
 		{
 			if (i + 1 == MAX_CAM_PARAMS / 2)
@@ -39,7 +37,6 @@ static bool	rt_scam(Environment *env, string line)
 		}
 		else
 			IFDOR(*line, ft_strdel(&tmp), 0);
-	}
 	ft_strdel(&tmp);
 	return (true);
 }
