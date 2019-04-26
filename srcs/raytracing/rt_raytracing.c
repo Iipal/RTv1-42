af240e9f8dfc30749471bf34d61bfed432445e06
Iipal <malkoleyplay@gmail.com>
2019-04-26 20:40:01 +0300

    nomre ?

--- srcs/raytracing/rt_raytracing.c
+++ srcs/raytracing/rt_raytracing.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/19 19:23:36 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/04/20 14:32:14 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/04/26 20:38:22 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ static inline bool	add_inter(Environment *env, Vec d, fDot *t, int32_t i)
 	return (true);
 }
 
-static inline Color	light(Color clr, Vec p, Vec n, Light l)
+static inline Color	add_compute_light(Color clr, Vec p, Vec n, Light l)
 {
 	double			intensity;
 	Color			tmp;
@@ -37,41 +37,47 @@ static inline Color	light(Color clr, Vec p, Vec n, Light l)
 	if (.0f < dot)
 	{
 		intensity += l.intensity * dot / (VLEN(n) * VLEN(x));
-		clr = *sdl_clrs_add(&clr, *sdl_clr_mul(&tmp, intensity));
+		sdl_clrs_add(&clr, *sdl_clr_mul(&tmp, intensity));
 	}
 	return (clr);
+}
+
+static inline Color	add_calculate_light(Environment *env, int32_t i, Vec d)
+{
+	t_lhelp	lh;
+
+	if (env->s.ins_objs <= i)
+		i = env->s.ins_objs - 1;
+	lh.cd = (Vec){env->s.cobj * d.x,
+				env->s.cobj * d.y,
+				env->s.cobj * d.z};
+	lh.p = (Vec){env->s.cam.pos.x + lh.cd.x,
+				env->s.cam.pos.y + lh.cd.y,
+				env->s.cam.pos.z + lh.cd.z};
+	lh.n = (Vec){lh.p.x - env->s.objs[i].pos.x,
+				lh.p.y - env->s.objs[i].pos.y,
+				lh.p.z - env->s.objs[i].pos.z};
+	lh.n = (Vec){lh.n.x / VLEN(lh.n),
+				lh.n.y / VLEN(lh.n),
+				lh.n.z / VLEN(lh.n)};
+	return (add_compute_light(env->s.objs[i].clr, lh.p, lh.n, env->s.l));
 }
 
 static inline Color	add_trace_ray(Environment *env, Vec d)
 {
 	fDot	t;
 	bool	is_figure;
-	t_lhelp	lh;
 	int32_t	i;
-	Color	ret;
 
 	i = -1;
-	ret = (Color){0, 0, 0};
-	while (++i < env->s.ins_objs)
+	while (env->s.ins_objs > ++i)
 		if ((is_figure = add_inter(env, d, &t, i)))
 		{
 			IFDO(t.x >= TMIN && t.x <= TMAX, env->s.cobj = t.x);
 			IFDO(t.y >= TMIN && t.y <= TMAX, env->s.cobj = t.y);
 			break ;
 		}
-	if (is_figure)
-	{
-		IFDO(env->s.ins_objs <= i, i = env->s.ins_objs - 1);
-		lh.cd = (Vec){env->s.cobj * d.x, env->s.cobj * d.y, env->s.cobj * d.z};
-		lh.p = (Vec){env->s.cam.pos.x + lh.cd.x,
-		env->s.cam.pos.y + lh.cd.y, env->s.cam.pos.z + lh.cd.z};
-		lh.n = (Vec){lh.p.x - env->s.objs[i].pos.x,
-		lh.p.y - env->s.objs[i].pos.y, lh.p.z - env->s.objs[i].pos.z};
-		lh.n = (Vec){lh.n.x / VLEN(lh.n),
-			lh.n.y / VLEN(lh.n), lh.n.z / VLEN(lh.n)};
-		ret = light(env->s.objs[i].clr, lh.p, lh.n, env->s.l);
-	}
-	return (ret);
+	return (is_figure ? add_calculate_light(env, i, d) : (Color){0, 0, 0});
 }
 
 void				rt_raytracing(Environment *env)
--- srcs/rt_camera_movements.c
+++ srcs/rt_camera_movements.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 19:14:47 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/04/25 20:03:16 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/04/26 19:53:31 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,13 @@ inline void	rt_camera_speed(double *cam_s, bool is_speed_up, bool is_speed_down)
 		if (MOVE_BOOST_PERCENT_SUP > *cam_s)
 			*cam_s += MOVE_BOOST_PERCENT_STEP;
 	}
-	else
-		if (MOVE_BOOST_PERCENT_DEFAULT < *cam_s)
-			*cam_s -= MOVE_BOOST_PERCENT_STEP;
+	else if (MOVE_BOOST_PERCENT_DEFAULT < *cam_s)
+		*cam_s -= MOVE_BOOST_PERCENT_STEP;
 	if (is_speed_down)
 	{
 		if (MOVE_BOOST_PERCENT_SDOWN < *cam_s)
 			*cam_s -= MOVE_BOOST_PERCENT_STEP;
 	}
-	else
-		if (MOVE_BOOST_PERCENT_DEFAULT > *cam_s)
-			*cam_s += MOVE_BOOST_PERCENT_STEP;
+	else if (MOVE_BOOST_PERCENT_DEFAULT > *cam_s)
+		*cam_s += MOVE_BOOST_PERCENT_STEP;
 }
