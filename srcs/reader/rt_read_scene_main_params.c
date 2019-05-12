69cb3b9a365d8e79e0dc3a99f93c887b1e122409
Iipal <malkoleyplay@gmail.com>
2019-05-12 23:50:56 +0300

    X position param validating negative numbers

--- srcs/reader/rt_read_scene_main_params.c
+++ srcs/reader/rt_read_scene_main_params.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 13:32:10 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/12 10:37:24 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/12 22:02:49 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ inline bool	rt_scam(Scene *sc, string s, size_t *o)
 	IFM_F(E_DCAMERA, sc->cam.is);
 	s += ft_skip_to_blank(s);
 	IF_F(*s++ != ' ');
-	IF_F(!*s || !ft_isdigit(*s));
+	IF_F(!*s || (!ft_isdigit(*s) && *s != '-'));
 	IF_F(',' != *(s += ft_digits(X(sc->cam.pos) = ft_atoi(s))) || !*s++);
 	IF_F(',' != *(s += ft_digits(Y(sc->cam.pos) = ft_atoi(s))) || !*s++);
 	IF_F(' ' != *(s += ft_digits(Z(sc->cam.pos) = ft_atoi(s))) || !*s++);
@@ -35,7 +35,7 @@ inline bool	rt_slight(Scene *sc, string s, size_t *o)
 {
 	s += ft_skip_to_blank(s);
 	IF_F(*s++ != ' ');
-	IF_F(!*s || !ft_isdigit(*s));
+	IF_F(!*s || (!ft_isdigit(*s) && *s != '-'));
 	IF_F(',' != *(s += ft_digits(X(sc->l[*o].pos) = ft_atoi(s))) || !*s++);
 	IF_F(',' != *(s += ft_digits(Y(sc->l[*o].pos) = ft_atoi(s))) || !*s++);
 	IF_F(' ' != *(s += ft_digits(Z(sc->l[*o].pos) = ft_atoi(s))) || !*s++);
