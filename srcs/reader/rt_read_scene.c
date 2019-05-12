4164a046189c3fc1988c6b926cf012d1fd288035
Iipal <malkoleyplay@gmail.com>
2019-05-12 23:52:00 +0300

    used help struct and added ingore line symbol '#'

--- srcs/reader/rt_read_scene.c
+++ srcs/reader/rt_read_scene.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 16:47:30 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/12 14:40:58 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/12 22:14:48 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,7 @@ static bool	add_valid_saved_data(Scene *sc)
 	return (true);
 }
 
-static bool	add_parser(Scene *sc, string *str,
-	int16_t nline, size_t *o, size_t *l)
+static bool	add_parser(Scene *sc, string *str, t_pfhelp *pfh)
 {
 	const fns_parse	fns[] = {rt_scam, rt_ssphere};
 	const string	params[] = {FP_CAM, FP_SPHERE};
@@ -47,18 +46,20 @@ static bool	add_parser(Scene *sc, string *str,
 	size_t			i;
 
 	i = ~0L;
+	++pfh->nline;
 	is_valid_data = false;
 	is_known_obj_type = false;
+	IFR(**str == '#', true);
 	if (!ft_strncmp(*str, FP_LIGHT, ft_strlen(FP_LIGHT))
 	&& (is_known_obj_type = true))
-		is_valid_data = rt_slight(sc, *str, l);
+		is_valid_data = rt_slight(sc, *str, &pfh->light_counter);
 	else
 		while ((sizeof(params) / sizeof(*params)) > ++i)
 			if (!ft_strncmp(*str, params[i], ft_strlen(params[i]))
 			&& (is_known_obj_type = true))
-				is_valid_data = fns[i](sc, *str, o);
+				is_valid_data = fns[i](sc, *str, &pfh->obj_counter);
 	IFDOM(E_OBJ, !is_known_obj_type, is_valid_data = false);
-	NO(E_ISYNTAX, is_valid_data, ERRAT(*str, nline), false);
+	NO(E_ISYNTAX, is_valid_data, ERRAT(*str, pfh->nline), false);
 	ft_strdel(str);
 	return (is_valid_data);
 }
@@ -70,10 +71,13 @@ static bool	add_valid_objs_counter(int32_t *fd, Scene *s, const string file)
 
 	while (0 < ft_gnl(*fd, &temp))
 	{
-		if (!ft_strncmp(temp, FP_LIGHT, ft_strlen(FP_LIGHT)))
-			++s->ins_l;
-		else if (ft_is_one_of_str(temp, max_objs, FP_SPHERE))
-			++s->ins_objs;
+		if (*temp != '#')
+		{
+			if (!ft_strncmp(temp, FP_LIGHT, ft_strlen(FP_LIGHT)))
+				++s->ins_l;
+			else if (ft_is_one_of_str(temp, max_objs, FP_SPHERE))
+				++s->ins_objs;
+		}
 		ft_strdel(&temp);
 	}
 	close(*fd);
@@ -90,20 +94,15 @@ bool		rt_read_scene(Environment *env, string scene_file)
 {
 	int32_t		fd;
 	string		tmp;
-	uint16_t	nline;
-	size_t		obj_counter;
-	size_t		light_counter;
+	t_pfhelp	pfh;
 
-	nline = 0;
-	obj_counter = 0;
-	light_counter = 0;
+	C(t_pfhelp, &pfh, 1);
 	IFDOMR(PERR, 0 > (fd = open(scene_file, O_RDONLY)), rt_free(&env), false);
 	NODO_F(add_valid_objs_counter(&fd, &env->s, scene_file), rt_free(&env));
 	while (0 < ft_gnl(fd, &tmp))
-		NODO_F(add_parser(&env->s, &tmp, ++nline, &obj_counter, &light_counter),
-			rt_free(&env));
+		NODO_F(add_parser(&env->s, &tmp, &pfh), rt_free(&env));
 	close(fd);
-	NO(E_EFILE, nline, rt_free(&env), false);
+	NO(E_EFILE, pfh.nline, rt_free(&env), false);
 	NO_F(add_valid_saved_data(&env->s));
 	return (true);
 }
