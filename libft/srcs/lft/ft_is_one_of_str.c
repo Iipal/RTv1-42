50c6d77924191bd3c62512f7d9342e2b75e4c1de
Iipal <malkoleyplay@gmail.com>
2019-05-13 20:50:13 +0300

    added bool for is needed compare strings lenght

--- libft/srcs/lft/ft_is_one_of_str.c
+++ libft/srcs/lft/ft_is_one_of_str.c
@@ -6,25 +6,33 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 23:08:35 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/12 14:36:44 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/13 20:05:41 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-bool	ft_is_one_of_str(cstring cmp, size_t n, ...)
+bool	ft_is_one_of_str(cstring cmp, size_t n, bool cmp_len, ...)
 {
 	va_list	ap;
 	bool	is_one_of;
 	char	*temp;
 
 	is_one_of = false;
-	va_start(ap, n);
+	va_start(ap, cmp_len);
 	while (n--)
 	{
 		temp = va_arg(ap, char*);
-		if (!ft_strncmp(cmp, temp, ft_strlen(temp)))
+		if (true == cmp_len && !ft_strncmp(cmp, temp, ft_strlen(temp)))
+		{
 			is_one_of = true;
+			break ;
+		}
+		else if (false == cmp_len && !ft_strcmp(cmp, temp))
+		{
+			is_one_of = true;
+			break ;
+		}
 	}
 	va_end(ap);
 	return (is_one_of);
