2f4e72f6e6a5a04050276c3ac381a039e4c8f273
Iipal <malkoleyplay@gmail.com>
2019-05-15 12:18:20 +0300

    added new utils funcs for validating rotate

--- srcs/rt_utils.c
+++ srcs/rt_utils.c
@@ -6,14 +6,14 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/16 14:44:49 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/14 20:00:55 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/15 12:15:48 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-inline bool		u_inrange(const Vector v,
-	const bool check_min, const bool check_max)
+inline bool		u_inrangev(const Vector v,
+				const bool check_min, const bool check_max)
 {
 	if (check_max
 	&& (MAX_X < X(v) || MAX_Y < Y(v) || MAX_Z < Z(v)))
@@ -24,12 +24,46 @@ inline bool		u_inrange(const Vector v,
 	return (true);
 }
 
-inline double_t	u_inrangev(const double_t v,
-	const bool check_min, const bool check_max)
+inline double_t	u_inranged(const double_t v,
+				const bool check_min, const bool check_max)
 {
 	if (check_max && MAX_X < v)
 		return (MAX_X);
 	if (check_min && MIN_X > v)
 		return (MIN_X);
 	return (v);
+}
+
+Vector			u_inrange_dir_max(Vector dir)
+{
+	Vector	out;
+
+	out = dir;
+	if (MAX_ROT_ANGLE < X(out))
+		while (MAX_ROT_ANGLE < X(out))
+			X(out) -= MAX_ROT_ANGLE;
+	if (MAX_ROT_ANGLE < Y(out))
+		while (MAX_ROT_ANGLE < Y(out))
+			Y(out) -= MAX_ROT_ANGLE;
+	if (MAX_ROT_ANGLE < Z(out))
+		while (MAX_ROT_ANGLE < Z(out))
+			Y(out) -= MAX_ROT_ANGLE;
+	return (out);
+}
+
+Vector			u_inrange_dir_min(Vector dir)
+{
+	Vector	out;
+
+	out = dir;
+	if (MIN_ROT_ANGLE > X(out))
+		while (MIN_ROT_ANGLE > X(out))
+			X(out) += MAX_ROT_ANGLE;
+	if (MIN_ROT_ANGLE > Y(out))
+		while (MIN_ROT_ANGLE > Y(out))
+			Y(out) += MAX_ROT_ANGLE;
+	if (MIN_ROT_ANGLE > Z(out))
+		while (MIN_ROT_ANGLE > Z(out))
+			Y(out) += MAX_ROT_ANGLE;
+	return (out);
 }
