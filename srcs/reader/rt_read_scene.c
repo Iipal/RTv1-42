561cc799cf52eec8cdb0b2d0db8475ff284b85cb
Iipal <malkoleyplay@gmail.com>
2019-05-15 12:19:15 +0300

    added validating rotate angle & other read scene file improvements

--- srcs/reader/rt_read_scene.c
+++ srcs/reader/rt_read_scene.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 16:47:30 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/14 17:19:22 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/15 10:54:53 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,25 @@ static bool	add_valid_saved_data(Scene *sc)
 	size_t	i;
 
 	NOM_F(E_NOCAM, sc->cam.is);
-	NOM_F(E_CAMDIR, u_inrange(sc->cam.dir, true, true));
-	NOM_F(E_CAMPOS, u_inrange(sc->cam.pos, true, true));
+	NOM_F(E_CAMPOS, u_inrangev(sc->cam.pos, true, true));
+	sc->cam.dir = u_inrange_dir_max(sc->cam.dir);
+	sc->cam.dir = u_inrange_dir_max(sc->cam.dir);
 	i = ~0L;
 	while (sc->ins_l > ++i)
 	{
-		NOM_F(E_LIGHTPOS, u_inrange(sc->l[i].pos, true, true));
+		NOM_F(E_LIGHTPOS, u_inrangev(sc->l[i].pos, true, true));
 		IFM_F(E_LINTENSE, .0f > sc->l[i].intens || 1.0f < sc->l[i].intens);
 	}
 	i = ~0L;
 	while (sc->ins_objs > ++i)
 	{
-		NOM_F(E_SPPOS, u_inrange(sc->objs[i].pos, true, true));
-		IFM_F(E_SPRAD, MIN_SPR > sc->objs[i].radius
+		NOM_F(E_OPOS, u_inrangev(sc->objs[i].pos, true, true));
+		IFM_F(E_ORAD, MIN_SPR > sc->objs[i].radius
 			|| MAX_SPR < sc->objs[i].radius);
-		IFM_F(E_SPSPEC, MIN_SPSP > sc->objs[i].spec
+		IFM_F(E_OSPEC, MIN_SPSP > sc->objs[i].spec
 			|| MAX_SPSP < sc->objs[i].spec);
+		sc->objs[i].dir = u_inrange_dir_max(sc->objs[i].dir);
+		sc->objs[i].dir = u_inrange_dir_min(sc->objs[i].dir);
 	}
 	return (true);
 }
@@ -71,13 +74,14 @@ static bool	add_valid_objs_counter(int32_t *fd, Scene *s, const string file)
 
 	while (0 < ft_gnl(*fd, &temp))
 	{
-		if (*temp == '#')
-			NULL;
-		else if (!ft_strncmp(temp, FP_LIGHT, ft_strlen(FP_LIGHT)))
-			++s->ins_l;
-		else if (ft_is_one_of_str(temp, true, FP_MAX_OBJS,
-					FP_SPHERE, FP_CONE, FP_CYLINDER, FP_PLANE))
-			++s->ins_objs;
+		if (*temp != '#')
+		{
+			if (!ft_strncmp(temp, FP_LIGHT, ft_strlen(FP_LIGHT)))
+				++s->ins_l;
+			else if (ft_is_one_of_str(temp, true, FP_MAX_OBJS,
+						FP_SPHERE, FP_CONE, FP_CYLINDER, FP_PLANE))
+				++s->ins_objs;
+		}
 		ft_strdel(&temp);
 	}
 	close(*fd);
