5b826ac5c63e4dc88031534330dc22a3933d87b4
Iipal <malkoleyplay@gmail.com>
2019-05-17 17:25:22 +0300

    improved sdl_pixelput(), removed unnecessary variable

--- libftsdl/srcs/sdl_pixelput.c
+++ libftsdl/srcs/sdl_pixelput.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 20:02:04 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/11 23:58:13 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/17 11:53:04 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,6 @@
 
 inline void	sdl_pixelput(SDL_Surface *surf, const Dot p, const Color clr)
 {
-	int32_t	*pixels;
-
-	if (surf)
-	{
-		pixels = surf->pixels;
-		if (0 <= p.x && 0 <= p.y && p.y < surf->h && p.x < surf->w)
-			pixels[p.y * surf->w + p.x] = SDL_CLR_TO_RGB(clr);
-	}
+	if (0 <= X(p) && 0 <= Y(p) && Y(p) < surf->h && X(p) < surf->w)
+		((uiarr)(surf->pixels))[Y(p) * surf->w + X(p)] = SDL_CLR_TO_RGB(clr);
 }
