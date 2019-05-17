819bc7bd6674a30737cf59eccdd9b8c6dcf7b8c8
Iipal <malkoleyplay@gmail.com>
2019-05-17 23:53:28 +0300

    inuse new named constants and fps based variables

--- srcs/sdl/rt_sdl_keys_events_lights.c
+++ srcs/sdl/rt_sdl_keys_events_lights.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/14 18:15:23 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/17 11:20:53 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/17 23:36:01 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,21 @@ static inline void	rt_light_zorintens(Environment *env, size_t i)
 	{
 		if (env->isr.is_zdec)
 			Z(env->s.l[i].pos) =
-				u_inranged(Z(env->s.l[i].pos) - env->fps.move * 5, 1, 0);
+				u_inranged(Z(env->s.l[i].pos) - env->fps.lights_move, 1, 0);
 		if (env->isr.is_zinc)
 			Z(env->s.l[i].pos) =
-				u_inranged(Z(env->s.l[i].pos) + env->fps.move * 5, 0, 1);
+				u_inranged(Z(env->s.l[i].pos) + env->fps.lights_move, 0, 1);
 	}
 	else
 	{
 		if (env->isr.is_zdec)
-			env->s.l[i].intens = (0.0f > env->s.l[i].intens - env->fps.move / 2)
-				? 0.0f : (env->s.l[i].intens - env->fps.move / 2);
+			env->s.l[i].intens =
+				(MIN_L_INTENS > env->s.l[i].intens - env->fps.lights_intens)
+				? MIN_L_INTENS : (env->s.l[i].intens - env->fps.lights_intens);
 		if (env->isr.is_zinc)
-			env->s.l[i].intens = (1.0f < env->s.l[i].intens + env->fps.move / 2)
-				? 1.0f : (env->s.l[i].intens + env->fps.move / 2);
+			env->s.l[i].intens =
+				(MAX_L_INTENS < env->s.l[i].intens + env->fps.lights_intens)
+				? MAX_L_INTENS : (env->s.l[i].intens + env->fps.lights_intens);
 	}
 }
 
@@ -43,16 +45,16 @@ void				rt_sdl_keys_events_light_debug(Environment *env)
 	{
 		if (env->isr.is_up)
 			Y(env->s.l[i].pos) =
-				u_inranged(Y(env->s.l[i].pos) - env->fps.move * 5, 1, 0);
+				u_inranged(Y(env->s.l[i].pos) - env->fps.lights_move, 1, 0);
 		if (env->isr.is_down)
 			Y(env->s.l[i].pos) =
-				u_inranged(Y(env->s.l[i].pos) + env->fps.move * 5, 0, 1);
+				u_inranged(Y(env->s.l[i].pos) + env->fps.lights_move, 0, 1);
 		if (env->isr.is_left)
 			X(env->s.l[i].pos) =
-				u_inranged(X(env->s.l[i].pos) - env->fps.move * 5, 1, 0);
+				u_inranged(X(env->s.l[i].pos) - env->fps.lights_move, 1, 0);
 		if (env->isr.is_right)
 			X(env->s.l[i].pos) =
-				u_inranged(X(env->s.l[i].pos) + env->fps.move * 5, 0, 1);
+				u_inranged(X(env->s.l[i].pos) + env->fps.lights_move, 0, 1);
 		rt_light_zorintens(env, i);
 	}
 }
--- srcs/sdl/rt_sdl_keys_events_objs.c
+++ srcs/sdl/rt_sdl_keys_events_objs.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/16 23:55:47 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/17 18:25:37 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/17 23:51:29 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,21 @@ static inline void	rt_objs_z_or_spec(Environment *env, size_t i)
 	{
 		if (env->isr.is_zdec)
 			Z(env->s.objs[i].pos) =
-				u_inranged(Z(env->s.objs[i].pos) - env->fps.move, 1, 0);
+				u_inranged(Z(env->s.objs[i].pos) + env->fps.move, false, true);
 		if (env->isr.is_zinc)
 			Z(env->s.objs[i].pos) =
-				u_inranged(Z(env->s.objs[i].pos) + env->fps.move, 1, 0);
+				u_inranged(Z(env->s.objs[i].pos) - env->fps.move, true, false);
 	}
 	else
 	{
 		if (env->isr.is_zdec)
 			env->s.objs[i].spec =
-				(MIN_SPSP > env->s.objs[i].spec - env->fps.move * 100)
-				? MIN_SPSP : (env->s.objs[i].spec - env->fps.move * 100);
+				(MIN_SPEC > env->s.objs[i].spec - env->fps.objs_spec_intens)
+				? MIN_SPEC : (env->s.objs[i].spec - env->fps.objs_spec_intens);
 		if (env->isr.is_zinc)
 			env->s.objs[i].spec =
-				(MAX_SPSP < env->s.objs[i].spec + env->fps.move * 100)
-				? MAX_SPSP : (env->s.objs[i].spec + env->fps.move * 100);
+				(MAX_SPEC < env->s.objs[i].spec + env->fps.objs_spec_intens)
+				? MAX_SPEC : (env->s.objs[i].spec + env->fps.objs_spec_intens);
 	}
 }
 
@@ -45,16 +45,16 @@ void				rt_sdl_keys_events_objs_debug(Environment *env)
 	{
 		if (env->isr.is_up)
 			Y(env->s.objs[i].pos) =
-				u_inranged(Y(env->s.objs[i].pos) - env->fps.move, 1, 0);
+				u_inranged(Y(env->s.objs[i].pos) - env->fps.move, true, false);
 		if (env->isr.is_down)
 			Y(env->s.objs[i].pos) =
-				u_inranged(Y(env->s.objs[i].pos) + env->fps.move, 0, 1);
+				u_inranged(Y(env->s.objs[i].pos) + env->fps.move, false, true);
 		if (env->isr.is_left)
 			X(env->s.objs[i].pos) =
-				u_inranged(X(env->s.objs[i].pos) - env->fps.move, 1, 0);
+				u_inranged(X(env->s.objs[i].pos) - env->fps.move, true, false);
 		if (env->isr.is_right)
 			X(env->s.objs[i].pos) =
-				u_inranged(X(env->s.objs[i].pos) + env->fps.move, 0, 1);
+				u_inranged(X(env->s.objs[i].pos) + env->fps.move, false, true);
 		rt_objs_z_or_spec(env, i);
 	}
 }
