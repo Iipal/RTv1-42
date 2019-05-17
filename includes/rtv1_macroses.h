eae4f47c33ebafa912d91547e45c2c8eb4c1777a
Iipal <malkoleyplay@gmail.com>
2019-05-17 23:48:48 +0300

    improved constant values naming

--- includes/rtv1_macroses.h
+++ includes/rtv1_macroses.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 14:12:55 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/17 22:27:26 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/17 23:47:27 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,24 +31,26 @@
 # define MIN_Z  -MAX_Z
 # define MIN_Y  -MAX_Y
 
-# define MAX_SPR    500
-# define MIN_SPR    0
+# define TMAX           10000.0f
+# define TMIN           0.001f
 
-# define MAX_SPSP   5000
-# define MIN_SPSP   0
+# define MAX_RADIUS     500
+# define MIN_RADIUS     0
+
+# define MAX_SPEC       10000
+# define MIN_SPEC       0
 
 # define MAX_ROT_ANGLE  360.0f
 # define MIN_ROT_ANGLE  -MAX_ROT_ANGLE
+
+# define MAX_LIGHTS     5
+# define MAX_L_INTENS   1.0f
+# define MIN_L_INTENS   .0f
 
 # define RT_CANVAS_STARTX   -(WIN_X / 2)
 # define RT_CANVAS_STARTY   -(WIN_Y / 2)
 # define RT_CANVAS_ENDX     WIN_X / 2
 # define RT_CANVAS_ENDY     WIN_Y / 2
-
-# define TMIN   0.001f
-# define TMAX   10000.0f
-
-# define MAX_LIGHTS 5
 
 # define MOVE_INC 5
 # define MOVE_SPEED_PERCENT_DEFAULT 1.0f
@@ -57,19 +59,23 @@
 # define MOVE_SPEED_PERCENT_STEP .05f
 
 # define RTV1_TITLE     "Ray Tracing v1"
+
 # define RTV1_EXE_PATH  "./RTv1"
 # define RTV1_FILEXT    ".rtv1"
+
 # define RTV1_USAGE     "text/usage.txt"
 # define RTV1_FHELP     "text/flags_usage.txt"
 
 # define FP_CAM         "Camera:"
 # define FP_LIGHT       "Light:"
+
+# define FP_MAX_OBJS    4
 # define FP_SPHERE      "Sphere:"
 # define FP_CONE        "Cone:"
 # define FP_PLANE       "Plane:"
 # define FP_CYLINDER    "Cylinder:"
-# define FP_MAX_OBJS    4
 
+# define MAX_FLAGS  4
 # define F_HELP     "--help"
 # define F_VPS      "--viewport-scale"
 # define F_SB       "--shadow-bright"
@@ -80,21 +86,18 @@
 # define SF_SB      "-sb"
 # define SF_FTC     "-ftc"
 
-# define MAX_FLAGS  4
-
 # define DEF_VIEWPORT_SCALE 1.0f
 # define DEF_SHADOW_BRIGHT  100.0f
 # define DEF_FPS_TEXT_COLOR 0x7FFF00
 
-# define REFRESH_FPS_COUNTER .1f
+# define REFRESH_FPS_COUNTER .25f
 
 # define VLEN(v) sqrt(X(v) * X(v) + Y(v) * Y(v) + Z(v) * Z(v))
 # define VDOT(v1, v2) (X(v1) * X(v2) + Y(v1) * Y(v2) + Z(v1) * Z(v2))
 # define VNORM(v) X(v)/sqrt(VDOT(v,v)),Y(v)/sqrt(VDOT(v,v)),Z(v)/sqrt(VDOT(v,v))
 # define VMUL(v, d) (Vector){X(v) * d, Y(v) * d, Z(v) * d}
 # define VMULR(v, d) (Vector){d * X(v), d * Y(v), d * Z(v)}
-
-# define SEKEY env->sdl->e.key.keysym.sym
+# define VDISC(k) (Y(k) * Y(k) - 4.0 * X(k) * Z(k))
 
 typedef bool	(*t_fn_fparse)(Flags*, char**, const size_t, size_t*);
 # define FNSCP typedef bool (*fns_parse)(Object*, char*);
