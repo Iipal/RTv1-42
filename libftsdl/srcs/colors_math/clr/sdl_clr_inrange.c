c3aa4bd0687921df216c75385ba0e47ab5c910d9
Iipal <malkoleyplay@gmail.com>
2019-05-17 17:26:33 +0300

    minor fixes

--- libftsdl/srcs/colors_math/clr/sdl_clr_inrange.c
+++ libftsdl/srcs/colors_math/clr/sdl_clr_inrange.c
@@ -6,17 +6,17 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 15:17:21 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/16 17:57:16 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/17 11:51:17 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftsdl_colors_math.h"
 
-inline int32_t	sdl_clr_inrange(const int32_t color)
+inline int32_t	sdl_clr_inrange(int32_t color)
 {
-	if (COLOR_MAX <= color)
+	if (COLOR_MAX < color)
 		return (COLOR_MAX);
-	else if (COLOR_MIN >= color)
+	if (COLOR_MIN > color)
 		return (COLOR_MIN);
-	return ((int32_t)color);
+	return (color);
 }
