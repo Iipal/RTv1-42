17a47fbdf7eb039df6a5a16bff0c9daed189a22a
Iipal <malkoleyplay@gmail.com>
2019-05-19 11:41:01 +0300

    updated macroses for correct work with new union

--- libftsdl/includes/libftsdl_colors_math_macroses.h
+++ libftsdl/includes/libftsdl_colors_math_macroses.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/14 12:22:29 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/17 12:38:21 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/19 11:36:26 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,17 @@
 # define COLOR_MAX  255
 # define COLOR_MIN  0
 
-# define RGB_BLACK  0x00
+# define RGB_BLACK  0x0
 # define RGB_WHITE  0xffffff
 
-# define CLR_BLACK (Color){0, 0, 0}
-# define CLR_WHITE (Color){255, 255, 255}
+# define CLR_BLACK (t_clr){0, 0, 0}
+# define CLR_WHITE (t_clr){255, 255, 255}
 
-# define INRANGE(c)	sdl_clr_inrange(c)
+# define U_CLR_BLACK (Color){RGB_BLACK}
+# define U_CLR_WHITE (Color){RGB_WHITE}
+
+# define INRANGE_MIN(c)	(COLOR_MIN > (c)) ? COLOR_MIN : (c)
+# define INRANGE(c)	(COLOR_MAX < (c)) ? COLOR_MAX : INRANGE_MIN(c)
 
 # define SDL_CLR_CMP(clr, u) (clr.r == u && clr.g == u && clr.b == u)
 # define SDL_CLRS_CMP(c1, c2) (c1.r == c2.r && c1.g == c2.g && c1.b == c2.b)
@@ -34,10 +38,10 @@
 # define SDL_RGB_TO_CLR(hex) (Color){hex >> 16, (hex >> 8)  & 0xff, hex & 0xff}
 # define SDL_HEX_TO_CLR(h)   (SDL_Color){h >> 16, (h >> 8)  & 0xff, h & 0xff, 0}
 
-# define SDL_CLR_ADD(c, a) (Color){INRANGE(c.r+a),INRANGE(c.g+a),INRANGE(c.b+a)}
-# define SDL_CLR_SUB(c, a) (Color){INRANGE(c.r-a),INRANGE(c.g-a),INRANGE(c.b-a)}
+# define SDL_CLR_ADD(c, a) (t_clr){INRANGE(c.r+a),INRANGE(c.g+a),INRANGE(c.b+a)}
+# define SDL_CLR_SUB(c, a) (t_clr){INRANGE(c.r-a),INRANGE(c.g-a),INRANGE(c.b-a)}
 
-# define COLOR  typedef struct s_color  Color;
+# define COLOR  typedef union u_color   Color;
 # define FCOLOR typedef struct s_fcolor FColor;
 
 #endif
