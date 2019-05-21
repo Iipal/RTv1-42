bc3fb2e1748995138727b8e8348d1f11d981374b
Iipal <malkoleyplay@gmail.com>
2019-05-21 23:33:44 +0300

    fixed normal objects calc

--- srcs/raytracing/rt_objects_normals.c
+++ srcs/raytracing/rt_objects_normals.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/16 19:17:13 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/21 18:56:20 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/21 23:33:29 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ inline Vector	rt_normal_sphere(const Vector p,
 								const Camera *const cam,
 								const void *const obj_ptr)
 {
-	(void)cam;
 	(void)d;
+	(void)cam;
 	return (p - ((Object*)obj_ptr)->pos);
 }
 
@@ -28,12 +28,11 @@ inline Vector	rt_normal_cone(const Vector p,
 							 	const void *const obj_ptr)
 {
 	const Object	*obj = (Object*)obj_ptr;
-	const Vector	div_t = cam->pos - d;
-	const double_t	t = VLEN(div_t);
-	const double_t	m = VDOT(d, obj->dir) * t + VDOT(p, obj->dir);
+	const Vector	x = cam->pos - obj->pos;
+	const double_t	m = VDOT(d, obj->dir) * cam->closes_t + VDOT(x, obj->dir);
 	const Vector	mul = VMUL(obj->dir, 1 + pow(tan(obj->radius / 2.0f), 2));
 
-	return ((p - obj->pos) - VMUL(mul, m));
+	return (p - obj->pos - VMUL(mul, m));
 }
 
 inline Vector	rt_normal_plane(const Vector p,
@@ -53,11 +52,10 @@ inline Vector	rt_normal_cylinder(const Vector p,
 								const Camera *const cam,
 								const void *const obj_ptr)
 {
-	const Object	*obj = (Object*)obj_ptr;
-	const Vector	x = cam->pos - obj->pos;
-	const Vector	div_t = cam->pos - d;
-	const double_t	t = VLEN(div_t);
-	const double_t	m = VDOT(d, obj->dir) * t + VDOT(x, obj->dir);
+	const Vector	obj_pos = ((Object*)obj_ptr)->pos;
+	const Vector	obj_dir = ((Object*)obj_ptr)->dir;
+	const Vector	x = cam->pos - obj_pos;
+	const double_t	m = VDOT(d, obj_dir) * cam->closes_t + VDOT(x, obj_dir);
 
-	return (p - obj->pos - VMUL(obj->dir, m));
+	return (p - obj_pos - VMUL(obj_dir, m));
 }
