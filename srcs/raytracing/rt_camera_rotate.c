09eb2a92560ecbc584c2eefc4b8907cd6c459191
Iipal <malkoleyplay@gmail.com>
2019-05-23 00:36:21 +0300

    fixed camera rotation formula

--- srcs/raytracing/rt_camera_rotate.c
+++ srcs/raytracing/rt_camera_rotate.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 13:46:41 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/22 14:38:59 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/23 00:21:30 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ inline Vector	rt_camera_rotate(Vector d, const Vector dir)
 	{
 		temp = Y(d);
 		Y(d) = temp * cos(X(dir)) + Z(d) * sin(X(dir));
-		Z(d) = -temp * sin(X(dir)) + Z(d) * cos(Y(dir));
+		Z(d) = -temp * sin(X(dir)) + Z(d) * cos(X(dir));
 	}
 	if (Y(dir))
 	{
