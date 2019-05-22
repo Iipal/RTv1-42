8be35ce01da3b6882a13c9c25e4a235fda3a53d5
Iipal <malkoleyplay@gmail.com>
2019-05-22 16:01:48 +0300

    idk. just do smtnhg

--- srcs/raytracing/rt_raytracing.c
+++ srcs/raytracing/rt_raytracing.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/14 19:54:55 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/22 15:11:15 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/22 15:54:42 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ inline Color		rt_raytracing(Environment *const env, Vector d)
 	d = u_vnorm(rt_camera_rotate(d, env->s.cam.dir));
 	obj = rt_closest_inter(env->s.cam.pos, d, env);
 	NO_R(obj, U_CLR_BLACK);
-	if (plane != obj->type)
-		obj->dir = u_vnorm(obj->dir);
+	obj->dir = u_vnorm(obj->dir);
 	h.p = env->s.cam.pos + u_vmul(d, env->s.cam.closes_t);
 	h.n = obj->fn_normal_calc(h.p, d, &env->s.cam, obj);
 	h.n = u_vnorm(h.n);
