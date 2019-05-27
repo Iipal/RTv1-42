f3d0a2c7c3a90078447039daa4a68842bfb1f7e4
Iipal <malkoleyplay@gmail.com>
2019-05-27 23:26:46 +0300

    added initialize for -frt

--- srcs/rt_init.c
+++ srcs/rt_init.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 15:22:19 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/22 23:35:45 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/27 22:59:21 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ inline bool	rt_init(Environment *env)
 	env->flags = (Flags){DEF_VIEWPORT_SCALE,
 						DEF_SHADOW_BRIGHT,
 						DEF_FPS_TEXT_COLOR, false,
+						DEF_FPS_REFRESH_TIMER, false,
 						DEF_DEBUG_MODE,
 						DEF_NOT_CALC_LIGHT};
 	return (true);
