eb8408dc18b36a501ed7a2ad976d3ff9aeb7f075
Iipal <malkoleyplay@gmail.com>
2019-05-27 23:24:42 +0300

    improved and added new errno messages for -frt

--- includes/rtv1_errno.h
+++ includes/rtv1_errno.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 14:05:25 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/25 17:12:57 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/27 22:58:28 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,29 +61,40 @@
 # define E_VPS      "--viewport-scale flag"
 # define E_SB       "--shadow-bright flag"
 # define E_FTC      "--fps-text-color flag"
+# define E_FRT      "--fps-refresh-time flag"
 # define E_NCL      "--no-calc-light flag"
+
+# define E_MAX_VPS  10
+# define E_MAX_SB   25
+# define E_MAX_FRT  500
 
 # define E_MISPARM  "Missed param for "
 # define E_MP_VPS   ERR E_MISPARM F_VPS "."
 # define E_MP_SB    ERR E_MISPARM F_SB "."
 # define E_MP_FTC   ERR E_MISPARM E_FTC "."
+# define E_MP_FRT   ERR E_MISPARM E_FRT "."
 
 # define E_UNSIGN   ERR "Only unsigned param values."
 # define E_DIGITS   ERR "Only digits in param values string."
 
-# define E_WVPSZERO ERR E_VPS " param is 0. Min is 1."
-# define E_WSBZERO  ERR E_SB " param is 0. Min is 1."
-# define E_WFTCZERO ERR E_FTC " param invalid or 0x0."
+# define E_VPS_ZERO  ERR E_VPS " param is 0. Min is 1."
+# define E_SB_ZERO   ERR E_SB " param is 0. Min is 1."
+# define E_FTC_ZERO  ERR E_FTC " param invalid or 0x0."
 
-# define E_MAX_10   " param greate than 25. Max is 25."
+# define E_MAX_10   " param greate than 10. Max is 10."
+# define E_MAX_25   " param greate than 25. Max is 25."
+# define E_MAX_500  " param greate than 500. Max is 500."
 
-# define E_WARNVPS  ERR E_VPS E_MAX_10
-# define E_WARNSB   ERR E_SB E_MAX_10
+# define E_WARN_VPS ERR E_VPS E_MAX_10
+# define E_WARN_SB  ERR E_SB E_MAX_25
+# define E_WARN_FRT ERR E_FRT E_MAX_500
 
 # define E_FTC0X    "0x"
 # define E_FTC_NO0X ERR "Missed \'0x\' for HEX color param in " E_FTC "."
 # define E_FTC_HEX  ERR "Invalid HEX color for " E_FTC "."
 
 # define E_USELESS_FTC WARNING E_FTC " useless because you don't use " E_DBG "."
-# define E_USELESS_SB WARNING E_SB " useles because you use " E_NCL "."
+# define E_USELESS_FRT WARNING E_FRT " useless because you don't use " E_DBG "."
+# define E_USELESS_SB  WARNING E_SB " useles because you use " E_NCL "."
+
 #endif
