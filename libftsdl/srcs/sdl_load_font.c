8adfc5edc78f1b60e8eb42e06d85e7aa3a62d11c
Iipal <malkoleyplay@gmail.com>
2019-05-29 18:20:57 +0300

    imprvoed font loading

--- libftsdl/srcs/sdl_load_font.c
+++ libftsdl/srcs/sdl_load_font.c
@@ -6,28 +6,25 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/19 19:15:15 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/15 12:52:49 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/29 11:29:20 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftsdl.h"
 
-SDL_Surface	*sdl_optimize_font_load(TTF_Font *font,
-				const string text,
+SDL_Surface	*sdl_load_font(const Sdl *const sdl,
+				const char *restrict const text,
 				const SDL_Color text_color,
-				const SDL_PixelFormat *format)
+				SDL_Surface *dst)
 {
-	SDL_Surface	*optimize_out;
-	SDL_Surface	*temp_load;
+	SDL_Surface	*temp;
 
-	optimize_out = NULL;
-	temp_load = NULL;
-	NO(TTF_GetError(),
-		temp_load = TTF_RenderText_Solid(font, text, text_color),
-		exit(EXIT_FAILURE), NULL);
-	NO(SDL_GetError(),
-		optimize_out = SDL_ConvertSurface(temp_load, format, 0),
-		exit(EXIT_FAILURE), NULL);
-	SDL_FreeSurface(temp_load);
-	return (optimize_out);
+	temp = NULL;
+	dst = NULL;
+	NOM_R(TTF_GetError(),
+		temp = TTF_RenderText_Solid(sdl->font, text, text_color), NULL);
+	NOM_R(SDL_GetError(),
+		dst = SDL_ConvertSurface(temp, sdl->wsurf->format, 0), NULL);
+	SDL_FreeSurface(temp);
+	return (dst);
 }
