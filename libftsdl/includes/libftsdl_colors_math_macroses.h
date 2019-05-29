9063513e345689b7fc776234003bd7d40182148d
Iipal <malkoleyplay@gmail.com>
2019-05-29 18:21:31 +0300

    improved libftsdl colors math macroses

--- libftsdl/includes/libftsdl_colors_math_macroses.h
+++ libftsdl/includes/libftsdl_colors_math_macroses.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/14 12:22:29 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/19 11:36:26 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/29 13:30:22 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@
 # define U_CLR_BLACK (Color){RGB_BLACK}
 # define U_CLR_WHITE (Color){RGB_WHITE}
 
-# define INRANGE_MIN(c)	(COLOR_MIN > (c)) ? COLOR_MIN : (c)
-# define INRANGE(c)	(COLOR_MAX < (c)) ? COLOR_MAX : INRANGE_MIN(c)
+# define INRANGE_MIN(c)	((COLOR_MIN > (c)) ? COLOR_MIN : (c))
+# define INRANGE(c)	((COLOR_MAX < (c)) ? COLOR_MAX : INRANGE_MIN(c))
 
 # define SDL_CLR_CMP(clr, u) (clr.r == u && clr.g == u && clr.b == u)
 # define SDL_CLRS_CMP(c1, c2) (c1.r == c2.r && c1.g == c2.g && c1.b == c2.b)
@@ -34,14 +34,14 @@
 # define SDL_CLR_ISONE(clr, u) (clr.r == u || clr.g == u || clr.b == u)
 # define SDL_CLRS_ISONE(c1, c2) (c1.r == c2.r || c1.g == c2.g || c1.b == c2.b)
 
-# define SDL_CLR_TO_RGB(clr) (clr.r << 16 | clr.g << 8 | clr.b)
-# define SDL_RGB_TO_CLR(hex) (Color){hex >> 16, (hex >> 8)  & 0xff, hex & 0xff}
-# define SDL_HEX_TO_CLR(h)   (SDL_Color){h >> 16, (h >> 8)  & 0xff, h & 0xff, 0}
+# define SDL_TO_RGB(clr) (clr.r << 16 | clr.g << 8 | clr.b)
+# define SDL_TO_CLR(hex) (Color){.c={hex >> 16,(hex >> 8) & 0xff,hex & 0xff}}
+# define SDL_CLR(h) (SDL_Color){h.r, h.g, h.b, 0}
 
 # define SDL_CLR_ADD(c, a) (t_clr){INRANGE(c.r+a),INRANGE(c.g+a),INRANGE(c.b+a)}
 # define SDL_CLR_SUB(c, a) (t_clr){INRANGE(c.r-a),INRANGE(c.g-a),INRANGE(c.b-a)}
 
-# define COLOR  typedef union u_color   Color;
-# define FCOLOR typedef struct s_fcolor FColor;
+# define COLOR  typedef union u_color   Color
+# define FCOLOR typedef struct s_fcolor FColor
 
 #endif
