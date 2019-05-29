a93cf12921e19f7984e082522583aecf8d07a615
Iipal <malkoleyplay@gmail.com>
2019-05-29 18:22:36 +0300

    fixed ptotypes and added inlcude SDL_image.h

--- libftsdl/includes/libftsdl.h
+++ libftsdl/includes/libftsdl.h
@@ -6,18 +6,25 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 19:59:09 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/17 12:56:15 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/05/29 11:45:16 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTSDL_H
 # define LIBFTSDL_H
 
+# ifdef __APPLE__
+#  include "../frameworks/SDL2_image.framework/Headers/SDL_image.h"
+# endif
+# ifdef __linux__
+#  include <SDL2/SDL_image.h>
+# endif
+
 # include "libftsdl_colors_math.h"
 # include "libftsdl_macroses.h"
 # include "libftsdl_errno.h"
 
-struct				s_sdl
+struct		s_sdl
 {
 	SDL_Window	*w;
 	SDL_Surface	*wsurf;
@@ -37,15 +44,15 @@ SDL;
 **	\param height: Window height what will created.
 **	\param title: Title for window.
 */
-extern bool			sdl_init(Sdl *sdl,
-						const int32_t width,
-						const int32_t height,
-						const string title);
+extern bool	sdl_init(Sdl *sdl,
+				const int32_t width,
+				const int32_t height,
+				const string title);
 
 /*
 **	Free all data in Sdl*.
 */
-extern void			sdl_free(Sdl **sdl);
+extern void	sdl_free(Sdl **sdl);
 
 /*
 **	mlx_pixelput implementation on SDL2 for copy workflow from MLX library.
@@ -57,12 +64,15 @@ extern void			sdl_free(Sdl **sdl);
 **	Protected from segfault when x && y pixel doesnt exist on Surface,
 **	and if Surface doesnt exist too.
 */
-extern void			sdl_pixelput(SDL_Surface *surf,
-						const Dot p, const Color clr);
+extern void	sdl_pixelput(SDL_Surface *surf,
+				const Dot p, const Color clr);
 
-extern SDL_Surface	*sdl_optimize_font_load(TTF_Font *font,
-						const string text,
-						const SDL_Color text_color,
-						const SDL_PixelFormat *format);
+SDL_Surface	*sdl_load_image(const char *restrict const path,
+				const SDL_PixelFormat *format,
+				SDL_Surface *dst);
+SDL_Surface	*sdl_load_font(const Sdl *const sdl,
+				const char *restrict const text,
+				const SDL_Color text_color,
+				SDL_Surface *dst);
 
 #endif
