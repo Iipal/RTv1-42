e02afefdde7aec44323e12a7b137c155dfc6e21d
Iipal <malkoleyplay@gmail.com>
2019-06-03 09:56:03 +0300

    removed unnesseccary commentary & improved intersect calcs

--- srcs/raytracing/rt_intersection.c
+++ srcs/raytracing/rt_intersection.c
@@ -6,15 +6,11 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 11:06:30 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/05/29 17:57:32 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/06/03 09:53:29 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
-
-/*
-**	1 == obj->type if it's plane. For plane don't neccessary calc discriminant.
-*/
 
 static inline fDot	add_intersection(const Vector o,
 									const Vector d,
@@ -23,18 +19,25 @@ static inline fDot	add_intersection(const Vector o,
 	const Vector	x = o - obj->pos;
 	const Vector	k = obj->fn_inter_calc(x, d, obj);
 	const double_t	disc = VDISC(k);
+	fDot			out;
 
-	if (.0f > disc)
-		return ((fDot){ -1, -1 });
-	return ((fDot){ (-Y(k) + sqrt(disc)) / (2.0f * X(k)),
-					(-Y(k) - sqrt(disc)) / (2.0f * X(k)) });
+	out = (fDot){-1, -1};
+	if (.0f <= disc)
+		out = (fDot){ (-Y(k) + sqrt(disc)) / (2.0f * X(k)),
+					(-Y(k) - sqrt(disc)) / (2.0f * X(k)) };
+	return (out);
 }
 
-static inline fDot	add_plane_inter(const Vector d, const Vector o,
+static inline fDot	add_plane_inter(const Vector o, const Vector d,
 						const Object *restrict const obj)
 {
-	return ((fDot){!u_vdot(d, obj->dir) ? -1
-			: (-u_vdot(o - obj->pos, obj->dir) / u_vdot(d, obj->dir)), -1});
+	const double_t	d_dot_v = u_vdot(d, obj->dir);
+	fDot			out;
+
+	out = (fDot){-1, -1};
+	if (d_dot_v)
+		X(out) = -u_vdot(o - obj->pos, obj->dir) / d_dot_v;
+	return (out);
 }
 
 Object				*rt_closest_inter(const Vector o,
@@ -50,9 +53,10 @@ Object				*rt_closest_inter(const Vector o,
 	env->scene.cam.t = env->tmax;
 	while (++i < env->scene.ins_objs)
 	{
-		(plane == env->scene.objs[i].type)
-			? (t = add_plane_inter(d, o, &env->scene.objs[i]))
-			: (t = add_intersection(o, d, &env->scene.objs[i]));
+		if (plane == env->scene.objs[i].type)
+			t = add_plane_inter(o, d, &env->scene.objs[i]);
+		else
+			t = add_intersection(o, d, &env->scene.objs[i]);
 		if (X(t) > env->tmin && X(t) < env->tmax && X(t) < env->scene.cam.t)
 		{
 			env->scene.cam.t = X(t);
