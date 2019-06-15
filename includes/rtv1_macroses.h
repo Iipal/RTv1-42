0b8c421025aa889d47bc1c383387d68ea067b5fc
Iipal <malkoleyplay@gmail.com>
2019-06-15 13:48:16 +0300

    other improvements

--- includes/rtv1_macroses.h
+++ includes/rtv1_macroses.h
@@ -6,12 +6,14 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 14:12:55 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/06/14 18:19:09 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/06/15 09:35:26 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RTV1_MACROSES_H
 # define RTV1_MACROSES_H
+
+# define RTV1_TITLE "Ray Tracing v1"
 
 # ifdef __APPLE__
 #  define WIN_X 2000
@@ -23,15 +25,19 @@
 #  define WIN_Y 400
 # endif
 
+# define RTV1_EXE_PATH "./RTv1"
+# define RTV1_FILEXT   ".json"
+
+# define RTV1_USAGE    "text/usage.txt"
+# define RTV1_FUSAGE   "text/flags_usage.txt"
+
 # define RT_CANVAS_STARTX -(WIN_X / 2)
 # define RT_CANVAS_STARTY -(WIN_Y / 2)
 # define RT_CANVAS_ENDX   WIN_X / 2
 # define RT_CANVAS_ENDY   WIN_Y / 2
 
-# define MAX(a, b) (((a) > (b)) ? (a) : (b))
-
-# define TMAX           100000.0
-# define TMIN           0.000001
+# define TMAX 100000.0
+# define TMIN 0.000001
 
 # ifdef M_PI
 #  undef M_PI
@@ -42,19 +48,13 @@
 # define COS_RAD(angle) cos(TO_RAD(angle))
 # define SIN_RAD(angle) sin(TO_RAD(angle))
 
-# define MOVE_INC 5
+# define MOVE_INC                   5
 # define MOVE_SPEED_PERCENT_DEFAULT 1.0f
 # define MOVE_SPEED_PERCENT_SUP     3.5f
 # define MOVE_SPEED_PERCENT_SDOWN   .15f
 # define MOVE_SPEED_PERCENT_STEP    .05f
 
-# define RTV1_TITLE         "Ray Tracing v1"
 
-# define RTV1_EXE_PATH      "./RTv1"
-# define RTV1_FILEXT        ".json"
 
-# define RTV1_MAIN_USAGE    "Usage: ./RTv1 [flags-params] <scene_name>.rtv1"
-# define RTV1_USAGE         "text/usage.txt"
-# define RTV1_FUSAGE        "text/flags_usage.txt"
 
 #endif
