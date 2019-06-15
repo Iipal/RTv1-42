9d34e89fbf9e7c6eb18807ed3b54dbf177cf9f7b
Iipal <malkoleyplay@gmail.com>
2019-06-15 13:39:39 +0300

    renamed file & func to -> rt_parse_arr_to_vec.c

--- srcs/parser/rt_parse_arr_to_vec.c
+++ srcs/parser/rt_parse_arr_to_vec.c
@@ -1,27 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rt_parce_array_to_vec.c                            :+:      :+:    :+:   */
+/*   rt_parse_arr_to_vec.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/14 17:49:23 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/06/14 19:26:52 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/06/15 12:56:57 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-bool	rt_parce_array_to_vec(const JSON_Array *const arr, __v4df *const dst)
+bool	rt_parse_arr_to_vec(const JSON_Array *const arr,
+			__v4df *const dst,
+			const char *const obj_name,
+			const size_t parsing_obj_counter)
 {
 	size_t	i;
 
 	i = ~0UL;
-	IFM_F(E_IN_PARAM(param_name, E_VEC_SIZE),
-		DEF_VEC_SIZE != json_array_get_count(arr));
+	// (void)obj_name;
+	IFDO_F(DEF_VEC_SIZE != json_array_get_count(arr),
+		ERRIN_N(obj_name, parsing_obj_counter + 1, E_VEC_FRMT));
 	while (DEF_VEC_SIZE > ++i)
-		IFM_F(E_IN_PARAM(param_name, E_VEC_FRMT),
-			JSONNumber != json_value_get_type(json_array_get_value(arr, i)));
+		IFDO_F(JSONNumber != json_value_get_type(json_array_get_value(arr, i)),
+			ERRIN_N(obj_name, parsing_obj_counter + 1, E_VEC_FRMT));
 	*dst = (__v4df){json_array_get_number(arr, 0),
 					json_array_get_number(arr, 1),
 					json_array_get_number(arr, 2)};
