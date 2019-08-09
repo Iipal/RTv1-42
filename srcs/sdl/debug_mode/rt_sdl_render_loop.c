20b5e1194d456bf50182f2a9fc605fdee03f15ce
Iipal <malkoleyplay@gmail.com>
2019-08-09 08:13:39 +0300

    instead of SEKEY macro

--- srcs/sdl/debug_mode/rt_sdl_render_loop.c
+++ srcs/sdl/debug_mode/rt_sdl_render_loop.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/19 19:20:04 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/06/15 17:54:26 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/08/09 08:12:40 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,17 @@ void	rt_sdl_render_loop(Environment *restrict const env)
 				quit = true;
 			else if (env->sdl->e.type == SDL_KEYDOWN)
 			{
-				if (SEKEY == SDLK_ESCAPE)
+				if (env->sdl->e.key.keysym.sym == SDLK_ESCAPE)
 					quit = true;
 				else
 				{
-					rt_sdl_keys_press(&env->isr, SEKEY);
-					rt_sdl_keys_press_switcher_mode(&env->isr, SEKEY);
-					rt_sdl_keys_press_add_settings(env, SEKEY);
+					rt_sdl_keys_press(&env->isr, env->sdl->e.key.keysym.sym);
+					rt_sdl_keys_press_switcher_mode(&env->isr, env->sdl->e.key.keysym.sym);
+					rt_sdl_keys_press_add_settings(env, env->sdl->e.key.keysym.sym);
 				}
 			}
 			else if (env->sdl->e.type == SDL_KEYUP)
-				rt_sdl_keys_release(&env->isr, SEKEY);
+				rt_sdl_keys_release(&env->isr, env->sdl->e.key.keysym.sym);
 		rt_sdl_keys_events(env);
 		rt_rendering(env);
 		rt_fps(&env->fps, env->cam_speed);
