57d756ab194b0d511a86f6d3d5c10a47143220e4
Iipal <malkoleyplay@gmail.com>
2019-08-14 22:02:26 +0300

    updated fake loop for non-debug mode exeuction

--- srcs/sdl/rt_sdl_fake_render_loop.c
+++ srcs/sdl/rt_sdl_fake_render_loop.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 10:39:08 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/08/09 08:12:06 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/08/14 22:01:33 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,13 @@ void	rt_sdl_fake_render_loop(Environment *restrict const env)
 	quit = false;
 	rt_rendering(env);
 	while (!quit)
+	{
 		while (SDL_PollEvent(&env->sdl->e) > 0)
 			if (SDL_QUIT == env->sdl->e.type)
 				quit = true;
 			else if (SDL_KEYDOWN == env->sdl->e.type
 				&& SDLK_ESCAPE == env->sdl->e.key.keysym.sym)
 				quit = true;
+		SDL_UpdateWindowSurface(env->sdl->w);
+	}
 }
