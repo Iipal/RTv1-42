325aaa9c0d9ed7473cbdd807aacb330c971b3d13
Iipal <malkoleyplay@gmail.com>
2019-08-24 18:48:22 +0300

    some wiiieerd shiiiit

--- srcs/rendering/rt_rendering.c
+++ srcs/rendering/rt_rendering.c
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 20:11:30 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/08/24 18:27:48 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/08/24 18:46:50 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,18 @@ static void	draw_from_l_to_obj(Environment *restrict const env)
 
 	for (ssize_t i = env->scene.ins_lights; 0 <= --i;) {
 		for (ssize_t j = env->scene.ins_objs; 0 <= --j;) {
+			double const	dist_to_obj = v_len(l[i].pos - o[i].pos);
+
 			uv_l = (__v2df){X(l[i].pos) / (!Z(l[i].pos) ? 1.0 : Z(l[i].pos)),
 						Y(l[i].pos) / (!Z(l[i].pos) ? 1.0 : Z(l[i].pos))};
-			uv_o = (__v2df){X(o[j].pos) / (!Z(o[j].pos) ? 1.0 : Z(o[i].pos)),
-						Y(o[j].pos) / (!Z(o[j].pos) ? 1.0 : Z(o[i].pos))};
-			uv_l = D_FROM_CANVAS(X(uv_l), Y(uv_l), WIN_X, WIN_Y);
-			uv_o = D_FROM_CANVAS(X(uv_o), Y(uv_o), WIN_X, WIN_Y);
+			uv_o = (__v2df){X(o[j].pos) / (!Z(o[j].pos) ? 1.0 : Z(o[j].pos)),
+						Y(o[j].pos) / (!Z(o[j].pos) ? 1.0 : Z(o[j].pos))};
+
+			uv_l = D_FROM_CANVAS(X(uv_l) * dist_to_obj,
+								Y(uv_l) * dist_to_obj, WIN_X, WIN_Y);
+			uv_o = D_FROM_CANVAS(X(uv_o) * dist_to_obj,
+								Y(uv_o) * dist_to_obj, WIN_X, WIN_Y);
+
 			wu_draw_line(uv_l, uv_o, env->sdl->wsurf);
 		}
 	}
